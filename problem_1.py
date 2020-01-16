c3594d5e6e5dfd3550e3a10970d379245d0020f4
Andrew MacLachlan <andrew.maclachlan@mcsaatchiperformance.com>
2020-01-16 16:55:44 +0000

    Cleaning up function description in problem 1

--- problem_1.py
+++ problem_1.py
@@ -2,8 +2,8 @@
 import numpy as np
 
 def multiplesOf3and5(number) -> int:
-	''' Takes an integer as an input and returns the sum of all the multiples of 3 or 5 below the provided integer
-	'''
+	""" Takes an integer as an input and returns the sum of all the multiples of 3 or 5 below the provided integer """
+	
 	# Create lists of the multiples of 3 and 5 up to and including the provided integer
 	list_of_3s = list(range(3, number + 1, 3))
 	list_of_5s = list(range(5, number + 1, 5))
