23556c5cbe1a173f1369dd63ac5c9558022eae92
Andrew MacLachlan <andrew.maclachlan@mcsaatchiperformance.com>
2020-01-17 09:10:39 +0000

    Removed numpy as not needed.

--- problem_1.py
+++ problem_1.py
@@ -1,6 +1,3 @@
-# Packages
-import numpy as np
-
 def multiplesOf3and5(number) -> int:
 	""" Takes an integer as an input and returns the sum of all the multiples of 3 or 5 below the provided integer """
 	
@@ -9,7 +6,7 @@ def multiplesOf3and5(number) -> int:
 	list_of_5s = list(range(5, number + 1, 5))
 
 	# Sum the 2 lists
-	final_sum = np.sum(list_of_3s + list_of_5s)
+	final_sum = sum(list_of_3s + list_of_5s)
 
 	# Return the answer
 	return final_sum
