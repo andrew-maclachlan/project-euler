7bbb3aeacce4504e305320a84dee199331ec81f7
Andrew MacLachlan <andrew.maclachlan@mcsaatchiperformance.com>
2020-01-17 09:12:26 +0000

    Renamed variables for additional clarity and added comments.

--- problem_3.py
+++ problem_3.py
@@ -1,10 +1,17 @@
 def largestPrimeFactor(number) -> int:
 	"""Returns the largest prime factor of any given integer."""
 
-	i = 2
-	while i < number:
-		if number % i == 0:
-			return largestPrimeFactor(number / i)
-		i += 1
+	# Initialise a variable to try as a factor
+	factor_check = 2
+
+	# Find the smallest factor and divide the given integer by this smallest factor. Repeat with the result until the final, largest, factor is found.
+	while factor_check < number:
+		# If the division has no remainder then factor_check is factor.
+		if number % factor_check == 0:
+			# Repeat the check with the result of the number divided by the smallest factor.
+			return largestPrimeFactor(number / factor_check)
+		# If there is a remainder then factor_check is not a factor so increment by 1 and try the next highest integer.
+		else:
+			factor_check += 1
 	
 	return int(number)
