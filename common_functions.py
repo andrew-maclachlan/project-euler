4968eec73b662115b7b4eeb19cf91e334cbb651f
Andrew MacLachlan <andrew.maclachlan@mcsaatchiperformance.com>
2020-01-22 14:55:47 +0000

    Creating new version of problem 5 which is much more efficient. Adding a common function script for useful functions created for the project.

---
+++ common_functions.py
@@ -0,0 +1,25 @@
+def allPrimeFactors(number, prime_list = None) -> list:
+	"""Returns a list of all prime factors of a given integer."""
+
+	# Initialise List
+	if prime_list is None:
+		prime_list = []
+
+	# Initialise a variable to try as a factor
+	factor_check = 2
+
+	# Find the smallest factor and divide the given integer by this smallest factor. Repeat with the result until there is a list of all factors.
+	while factor_check <= number:
+		# If the division has no remainder then factor_check is factor.
+		if number % factor_check == 0:
+			# Append the factor to the list
+			prime_list.append(factor_check)
+
+			# Repeat the check with the result of the number divided by the smallest factor.
+			return allPrimeFactors(number / factor_check, prime_list)
+		# If there is a remainder then factor_check is not a factor so increment by 1 and try the next highest integer.
+		else:
+			factor_check += 1
+
+	# Return a list of prime factors
+	return prime_list
--- problem_5.py
+++ problem_5.py
@@ -1,25 +1,32 @@
+# Some other useful functions
+from common_functions import allPrimeFactors
+
 def smallestMult(n) -> int:
 	"""Returns the smallest positive integer that is evenly divisible by all the numbers from 1 to n."""
 
 	# Generate a list of integers from 1 to n
-	factor_list = list(range(n, 0, -1))
-	
-	# Generate maximum product. This will be the upper limit as it is all factor mulitplied together
-	max_product = 1
-	for factor in factor_list:
-		max_product *= factor
-	
-	# Get max factor from the list and set it as the first check
-	max_factor = max(factor_list)
-	multiple_check = max_factor
+	integer_list = list(range(1, n + 1))
 
-	# If the smallest multiple check becomes larger than the max_product then max_product is also the smallest mult
-	while multiple_check < max_product:
-		# Check whether each factor divides equally and if any doesn't the break the loop and move on the the next iteration. If all factors divide equally then return the checked integer.
-		for factor in factor_list:
-			if multiple_check % factor != 0:
-				multiple_check += max_factor
-				break
-		else:
-			return multiple_check
-	return max_productexit()
+	# List of factors that will provide the min product
+	final_list = []
+
+	# Loop through all the integers up to and including the provided n
+	for integer in integer_list:
+		# Return a list of prime factors of the current factor
+		prime_factors = allPrimeFactors(integer)
+
+		# Loop through each of the numbers in the unique list of prime factors
+		for number in set(prime_factors):
+			# Count the difference in the number of each prime factor against what is already in the final_list
+			diff_count = prime_factors.count(number) - final_list.count(number)
+
+			# Append the number of missing factors to the final list
+			for i in range(diff_count):
+				final_list.append(number)
+
+	# Calculate the product of the integers in the final list
+	max_product = 1
+	for factor in final_list:
+		max_product *= factor
+		
+	return max_product
