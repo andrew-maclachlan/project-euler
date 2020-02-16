ebf984389423a99fb3db332ee17a94f5bed096a0
unknown <andrew@MPL-12506.eu.mcsaatchi.pri>
2020-02-16 18:21:10 +0000

    Simplifying Problem 6

--- problem_6.py
+++ problem_6.py
@@ -1,22 +1,13 @@
 def sumSquareDifference(n) -> int:
-	""" Takes an integer (n) and returns the difference between the sum of the squares of the first n natural numbers and the square of the sum. """
+	""" Takes an integer n and returns the difference between the sum of the squares of the first n natural numbers and the square of the sum. """
 
 	# Sum the squares
 	sum_squares = 0
-	i = 1
-	while i <= n:
+	for i in range(1, n + 1):
 		sum_squares += i ** 2
-		i += 1
 
 	# Square the sums
-	square_sums = 0
-	j = 1
-	while j <= n:
-		square_sums += j
-		j += 1
-	square_sums = square_sums ** 2
+	square_sums = sum(range(1, n + 1)) ** 2
 
 	# Return the difference between the two
 	return square_sums - sum_squares
-
-	# Test comment
