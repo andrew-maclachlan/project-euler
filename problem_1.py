87fab3f4617894d00885cb4b96dec8ab9e711c39
Andrew MacLachlan <maclachlan.andrew@gmail.com>
2020-04-05 13:52:49 +0100

    Adding error handling.

--- problem_1.py
+++ problem_1.py
@@ -1,12 +1,13 @@
 def multiplesOf3and5(number) -> int:
-	""" Takes an integer as an input and returns the sum of all the multiples of 3 or 5 below the provided integer """
+	"""
+	Takes an integer as an input and returns the sum of all the multiples of 3 or 5 below the provided integer.
+	"""
+	if type(number) is not type(int()):
+		raise ValueError("Input must be of type int.")
 	
 	# Create lists of the multiples of 3 and 5 up to and including the provided integer
 	list_of_3s = list(range(3, number + 1, 3))
 	list_of_5s = list(range(5, number + 1, 5))
 
 	# Sum the 2 lists
-	final_sum = sum(list_of_3s + list_of_5s)
-
-	# Return the answer
-	return final_sum
+	return sum(list_of_3s + list_of_5s)
