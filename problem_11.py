26c96f103ac34052a04325c9488ffbbbe0e69dcb
Andrew MacLachlan <maclachlan.andrew@gmail.com>
2020-05-09 11:53:49 +0100

    Renaming max product return in sub_grid_sweep for more clarity.

--- problem_11.py
+++ problem_11.py
@@ -6,34 +6,34 @@ def largestGridProduct(grid, n) -> int:
 
 	max_product = 0
 
-	def sub_grid_sweep(grid, max_product = 0) -> int:
+	def sub_grid_sweep(grid, sub_max_product = 0) -> int:
 		# Horizontal Sweep
 		for i in grid:
 			product = listProduct(i)
-			if product > max_product:
-				max_product = product
+			if product > sub_max_product:
+				sub_max_product = product
 
 		# Vertical Sweep
 		for i in zip(*grid[::-1]):
 			product = listProduct(i)
-			if product > max_product:
-				max_product = product
+			if product > sub_max_product:
+				sub_max_product = product
 
 		# Diagonal \ lr
 		diag_sum_lr = 1
 		for i,j in enumerate(grid):
 			diag_sum_lr *= j[i]
-			if diag_sum_lr > max_product:
-				max_product = diag_sum_lr
+			if diag_sum_lr > sub_max_product:
+				sub_max_product = diag_sum_lr
 
 		# Diagonal / rl
 		diag_sum_rl = 1
 		for i,j in enumerate(grid):
 			diag_sum_rl *= j[-(i+1)]
-			if diag_sum_rl > max_product:
-				max_product = diag_sum_rl
+			if diag_sum_rl > sub_max_product:
+				sub_max_product = diag_sum_rl
 
-		return max_product
+		return sub_max_product
 
 	for row in range(0, len(grid) - n + 1):
 		for col in range(0, len(grid) - n + 1):
