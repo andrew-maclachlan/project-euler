9672618eb1bf5fb950ed50b52804f802b22bb024
Andrew MacLachlan <maclachlan.andrew@gmail.com>
2025-02-10 17:38:45 +0000

    Up to date finally

--- projecteuler/projecteuler/__init__.py
+++ projecteuler/projecteuler/__init__.py
@@ -2,23 +2,37 @@
 
 from .common_functions import all_prime_factors, is_prime
 
-from .problem_1 import problem_1, multiples_of_3_and_5
-from .problem_2 import problem_2, fibo_even_sum
-from .problem_3 import problem_3, largest_prime_factor
-from .problem_4 import problem_4, largest_palindrome_product
-from .problem_5 import problem_5, smallest_mult
-from .problem_6 import problem_6, sum_square_difference
-from .problem_7 import problem_7, nth_prime
-from .problem_8 import problem_8, largest_product_in_a_series
-from .problem_9 import problem_9, special_pythagorean_triplet
-from .problem_10 import problem_10, prime_summation
-from .problem_11 import problem_11, largest_grid_product
-from .problem_12 import problem_12, divisible_triangle_number
-from .problem_13 import problem_13, largeSum
+from .problem_1 import multiples_of_3_and_5
+from .problem_2 import fibo_even_sum
+from .problem_3 import largest_prime_factor
+from .problem_4 import largest_palindrome_product
+from .problem_5 import smallest_mult
+from .problem_6 import sum_square_difference
+from .problem_7 import nth_prime
+from .problem_8 import largest_product_in_a_series
+from .problem_9 import special_pythagorean_triplet
+from .problem_10 import prime_summation
+from .problem_11 import largest_grid_product
+from .problem_12 import divisible_triangle_number
+from .problem_13 import largeSum
+
+problem_1 = multiples_of_3_and_5
+problem_2 = fibo_even_sum
+problem_3 = largest_prime_factor
+problem_4 = largest_palindrome_product
+problem_5 = smallest_mult
+problem_6 = sum_square_difference
+problem_7 = nth_prime
+problem_8 = largest_product_in_a_series
+problem_9 = special_pythagorean_triplet
+problem_10 = prime_summation
+problem_11 = largest_grid_product
+problem_12 = divisible_triangle_number
+problem_13 = largeSum
 
 __all__ = [
     'all_prime_factors', 'is_prime',
-    'problem_1', 'multiples_of_3_and_5', 
+    'problem_1', 'multiples_of_3_and_5',
     'problem_2', 'fibo_even_sum',
     'problem_3', 'largest_prime_factor',
     'problem_4', 'largest_palindrome_product',
--- projecteuler/tests/test_problem_13.py
+++ projecteuler/tests/test_problem_13.py
@@ -1,7 +1,7 @@
 """Tests for Problem 13.
 """
 
-from ..problems import largeSum
+from projecteuler import largeSum
 
 testNums = [
     37107287533902102798797998220837590246510135740250,
